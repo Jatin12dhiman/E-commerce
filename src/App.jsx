8c70b2c59885443e7fe0efc79333e5cb4d57fbe4
Jatin Dhiman <dhimanjatin003@gmail.com>
2024-11-20 20:20:19 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Navbar from "./Components/Navbar"
+import Footer from "./Components/Footer"
+import Home from "./Pages/Home"
+import './index.css'
+function App() {
+
+  return (
+    // <BrowserRouter>
+    //  <Navbar />
+    //     <Routes>
+    //       <Route path="/" element={<Home />}></Route>
+    //     </Routes>
+    //    <Footer /> 
+    // </BrowserRouter>
+    <>
+      <h1 className="bg-black">Hllo</h1>
+    </>
+  )
+}
+
+export default App
